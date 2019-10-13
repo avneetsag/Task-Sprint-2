34140e91622c9e867c0bd06f614937e6b98abed4
avneetsag <52964154+avneetsag@users.noreply.github.com>
2019-10-13 16:53:45 +1100

    Add files via upload

---
+++ rat_trap_l.ino
@@ -0,0 +1,33 @@
+
+int led = 6;
+int pir = 5;
+int buzzer = 4;
+int catchpin = 2;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  pinMode(pir, INPUT);
+  pinMode(pirs, INPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(catchPin, OUTPUT);
+}
+
+void loop()
+{
+  if(digitalRead(5)== HIGH )
+    {
+      Serial.println("Object Detected");
+      digitalWrite(led, HIGH);
+      analogWrite(buzzer, 200);
+      digitalWrite(catchPin, HIGH);
+    } 
+ else
+  {
+    Serial.println("No Object Detected");
+    digitalWrite(led, LOW);
+    digitalWrite(buzzer, LOW);
+    digitalWrite(catchPin, LOW);
+  }
+   delay(1000);
+}
